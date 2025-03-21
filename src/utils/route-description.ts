c3a53d58ed3ec60d60d61bfb36a089870f925895
Debora Serra <debora.r.serra@gmail.com>
2025-03-21 10:27:39 -0700

    feat: add interface for router and descriptions for header

---
+++ src/utils/route-description.ts
@@ -0,0 +1,20 @@
+const routeDescription = {
+  dashboard: {
+    title: "Dashboard",
+    description: "See your Crawler status",
+  },
+  playground: {
+    title: "Playground",
+    description: "Crawl pages, retrieve data and save them here",
+  },
+  "logs-data": {
+    title: "Logs & data",
+    description: "View past crawlings and download data",
+  },
+  settings: {
+    title: "Settings",
+    description: "Configure your Crawler",
+  },
+};
+
+export default routeDescription;
