56d5fd58b5eca942dc3a4b3dd1e0b86b48c9cc14
Debora Serra <debora.r.serra@gmail.com>
2025-03-21 10:28:05 -0700

    feat: add header with hard coded style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { RoutesType } from "@components/utils/interfaces";
+import routeDescription from "@components/utils/route-description";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 import style from "./header.module.scss";
 
 const Header = () => {
-  return <div className={style.header}>Header</div>;
+  let pathname = usePathname();
+
+  if (pathname === "/") {
+    pathname = "/dashboard";
+  }
+
+  const info = routeDescription[pathname.replace("/", "") as RoutesType];
+
+  return (
+    <div className={style.header}>
+      <div className={style.header__left}>
+        <h1 className={style["header__left--title"]}>{info.title}</h1>
+        <p className={style["header__left--desc"]}>{info.description}</p>
+      </div>
+      <div className={style.header__right}>
+        <Button asChild variant={"secondary"} style={{ padding: "10px 24px" }}>
+          <a
+            className={style["header__right--btn"]}
+            href='https://discord.com/'
+          >
+            Discord
+          </a>
+        </Button>
+        <Button asChild style={{ padding: "10px 24px" }}>
+          <a
+            className={style["header__right--btn"]}
+            href='https://github.com/bluewave-labs/rockscraper'
+          >
+            Documentation
+          </a>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
