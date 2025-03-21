ba45e0f90e54573b01584bd5c76ab3b2d26fd1a4
Debora Serra <debora.r.serra@gmail.com>
2025-03-21 08:55:12 -0700

    chore: update tailwind config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["placecats.com"],
+    remotePatterns: [{
+      hostname: "placecats.com"
+    }],
   },
   devIndicators: false
 };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import { colors } from "./src/style/colors";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/lib/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+};
