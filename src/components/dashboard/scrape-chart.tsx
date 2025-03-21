d992be6867354c62b8132e0dfeb74f08ddd69378
Debora Serra <debora.r.serra@gmail.com>
2025-03-21 14:16:44 -0700

    feat: create scrape graph card

---
+++ src/components/dashboard/scrape-chart.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { Card, CardContent, CardTitle } from "@src/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@src/components/ui/chart";
+import { colors } from "@src/utils/colors";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "July", desktop: 160, mobile: 100 },
+  { month: "August", desktop: 200, mobile: 150 },
+  { month: "September", desktop: 180, mobile: 110 },
+  { month: "October", desktop: 150, mobile: 90 },
+  { month: "November", desktop: 250, mobile: 160 },
+  { month: "December", desktop: 300, mobile: 180 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: colors.blue[300],
+  },
+  mobile: {
+    label: "Mobile",
+    color: colors.zinc[800],
+  },
+} satisfies ChartConfig;
+
+const ScrapeChart = () => {
+  return (
+    <Card>
+      <CardTitle>Scraped pages</CardTitle>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey='month'
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar
+              dataKey='desktop'
+              stackId='a'
+              fill={colors.blue[300]}
+              radius={[4, 4, 0, 0]}
+            />
+            <Bar
+              dataKey='mobile'
+              stackId='a'
+              fill={colors.zinc[800]}
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ScrapeChart;
