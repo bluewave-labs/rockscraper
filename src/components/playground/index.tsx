8c4f9635b71b7f116d754846328004d905ba5222
Debora Serra <debora.r.serra@gmail.com>
2025-03-25 14:48:24 -0700

    feat: add use ai checkbox

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from "../ui/card";
+import Checkbox from "../ui/checkbox";
 import Cookies from "./cookies";
 import Headers from "./headers";
 import style from "./playground.module.scss";
@@ -10,6 +11,14 @@ const Playground = () => {
         <h2 className={style.play__title}>Fine tune your query</h2>
         <Headers />
         <Cookies />
+        <div className="mt-10">
+          <h2 className={style.play__title}>Output</h2>
+          <label htmlFor='use_ai' className={style['play__checkbox--label']}>
+            <Checkbox id='use_ai' />
+            <span>Make an AI query on the data retrieved. This will be applied to all
+            pages.</span>
+          </label>
+        </div>
       </div>
       <Card className={style.play__right}></Card>
     </div>
--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,14 +1,23 @@
 import { cn } from "@src/lib/utils";
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
-const Checkbox = () => {
+const Checkbox = ({
+  className,
+  ...props
+}: DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>) => {
   return (
     <input
       type='checkbox'
       className={cn(
-        "w-0 h-0 relative",
-        "after:w-5 after:h-5 after:absolute after:top-0 after:left-0 checked:after:bg-blue-500 after:border after:border-blue-500 after:rounded-sm after:cursor-pointer",
+        "w-5 h-5 relative appearance-none rounded-sm",
+        "after:w-5 after:h-5 after:absolute after:top-0 after:left-0 checked:after:bg-blue-500 checked:after:border-blue-500 after:border after:border-zinc-800 bg-zinc-800 after:rounded-sm after:cursor-pointer",
         "before:w-2 before:h-3.5 before:absolute before:top-[1px] before:left-[6px] checked:before:border-r-2 checked:before:border-b-2 before:border-blue-200 before:z-10 before:rotate-45",
+        className
       )}
+      {...props}
     />
   );
 };
