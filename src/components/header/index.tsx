54af7c59cd4e4c30691b617cd16613e35525be17
Debora Serra <debora.r.serra@gmail.com>
2025-04-01 11:42:37 -0700

    style: update style on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,50 +1,39 @@
-"use client";
-import { RoutesType } from "@src/utils/interfaces";
-import { DISCORD, DOCUMENTATION } from "@src/utils/links";
-import routeDescription from "@src/utils/route-description";
-import { usePathname } from "next/navigation";
-import { Button } from "../ui/button";
-import style from "./header.module.scss";
-import { useIsMobile } from "@src/hooks/use-mobile";
-import { SidebarTrigger } from "../ui/sidebar";
+'use client';
+import { useIsMobile } from '@src/hooks/use-mobile';
+import { RoutesType } from '@src/utils/interfaces';
+import { DISCORD, DOCUMENTATION } from '@src/utils/links';
+import routeDescription from '@src/utils/route-description';
+import { usePathname } from 'next/navigation';
+import { Button } from '../ui/button';
+import style from './header.module.scss';
 
 const Header = () => {
   const isMobile = useIsMobile();
   let pathname = usePathname();
 
-  if (pathname === "/") {
-    pathname = "/dashboard";
+  if (pathname === '/') {
+    pathname = '/dashboard';
   }
 
-  const info = routeDescription[pathname.replace("/", "") as RoutesType] || {
-    title: "Not Found",
+  const info = routeDescription[pathname.replace('/', '') as RoutesType] || {
+    title: 'Not Found',
     description: "This page doesn't exist",
   };
 
   return (
-    <div className={style.header}>
+    <div className={`glassy-background-strong border-b border-b-gray-0/20 min-h-[64px] flex items-center justify-between px-4 py-2.5`}>
       <div className={style.header__left}>
-        <h1 className={style["header__left--title"]}>{info.title}</h1>
-        <p className={style["header__left--desc"]}>{info.description}</p>
+        <h1 className={style['header__left--title']}>{info.title}</h1>
+        <p className={style['header__left--desc']}>{info.description}</p>
       </div>
       <div className={style.header__right}>
-        <Button asChild variant={"secondary"} size={isMobile ? "xs" : "default"}>
-          <a
-            className={style["header__right--btn"]}
-            href={DISCORD}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
+        <Button asChild variant={'secondary'} size={isMobile ? 'xs' : 'default'}>
+          <a className={style['header__right--btn']} href={DISCORD} target="_blank" rel="noopener noreferrer">
             Discord
           </a>
         </Button>
-        <Button asChild size={isMobile ? "xs" : "default"}>
-          <a
-            className={style["header__right--btn"]}
-            href={DOCUMENTATION}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
+        <Button asChild size={isMobile ? 'xs' : 'default'}>
+          <a className={style['header__right--btn']} href={DOCUMENTATION} target="_blank" rel="noopener noreferrer">
             Documentation
           </a>
         </Button>
