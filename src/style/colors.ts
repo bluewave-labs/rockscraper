7cf8fdd88d282c1c9f122c87c7404521ddcd68ef
Debora Serra <debora.r.serra@gmail.com>
2025-04-01 08:29:12 -0700

    feat: add gray colors

--- src/style/colors.ts
+++ src/style/colors.ts
@@ -1,34 +1,50 @@
 export const colors = {
   zinc: {
-    100: "#E2E2E9", //text
-    200: "#dce2f9", //text link
-    300: "#c5c6d0", //text
-    400: "#8F9099",
-    500: "#404659", // bg btn secondary
-    600: "#44464f", //line header
-    700: "#38393F",
-    800: "#33353A", // bg graph
-    900: "#1e1f25", //bg secondary
-    950: "#121318", //gb primary
+    100: '#E2E2E9', //text
+    200: '#dce2f9', //text link
+    300: '#c5c6d0', //text
+    400: '#8F9099',
+    500: '#404659', // bg btn secondary
+    600: '#44464f', //line header
+    700: '#38393F',
+    800: '#33353A', // bg graph
+    900: '#1e1f25', //bg secondary
+    950: '#121318', //gb primary
   },
 
   purple: {
-    200:"#FDD7FA",
-    700: "#593D5A", // bg button try
-    800: "#592c4f", // bg button primary
+    200: '#FDD7FA',
+    700: '#593D5A', // bg button try
+    800: '#592c4f', // bg button primary
   },
 
   green: {
-    500: "#3CB66D", //badge
-    600: "#1DA253"
+    500: '#3CB66D', //badge
+    600: '#1DA253',
   },
 
   blue: {
-    200: "#D9E2FF", // checkbox check
-    300: "#B0C6FF", // bg button primary
-    400: "#a0bfff", // bg button primary
-    500: "#2E4578", // checkbox bg
-    600: "#152E60", // btn primary text
-    900: '#282A2F'
+    200: '#D9E2FF', // checkbox check
+    300: '#B5C6F4', // bg button primary
+    400: '#a0bfff', // bg button primary
+    500: '#2E4578', // checkbox bg
+    600: '#152E60', // btn primary text
+    900: '#282A2F',
+  },
+
+  gray: {
+    100: '#010314',
+    95: '#0B0D1C',
+    90: '#171825',
+    85: '#212230',
+    80: '#2A2B3A',
+    70: '#383A4D',
+    60: '#5E6077',
+    50: '#9FA2B9',
+    40: '#BABCD2',
+    30: '#DFE1F4',
+    20: '#ECECFB',
+    10: '#F4F4FF',
+    0: '#FFFFFF',
   },
 };
