9e2451ea9d83b015d78692b6703db5b6d115863d
Debora Serra <debora.r.serra@gmail.com>
2025-04-08 12:06:08 -0700

    chore: minor updates on header and content

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -21,7 +21,9 @@ const Header = () => {
   };
 
   return (
-    <div className={`glassy-background-strong border-b border-b-gray-0/20 min-h-[64px] flex items-center justify-between px-4 py-2.5`}>
+    <div
+      className={`glassy-background-strong border-b border-b-gray-0/20 min-h-[64px] flex items-center justify-between px-6 md:px-4 py-2.5 max-w-screen`}
+    >
       <div className={style.header__left}>
         <h1 className={style['header__left--title']}>{info.title}</h1>
         <p className={style['header__left--desc']}>{info.description}</p>
