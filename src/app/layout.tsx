91dc28a8bb9beda3608c242f6ced0c79d2ab03e5
Debora Serra <debora.r.serra@gmail.com>
2025-04-15 13:57:58 -0700

    feat: add lib and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import '@bluewavelabs/prism-ui/style';
 import Content from '@src/components/content';
 import Header from '@src/components/header';
 import AppSidebar from '@src/components/sidebar';
-import { SidebarProvider } from '@src/components/ui/sidebar';
 import { Toaster } from '@src/components/ui/sonner';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
@@ -20,9 +20,7 @@ export default function RootLayout({ children }: { readonly children: React.Reac
   return (
     <html lang="en">
       <body className={`${roboto.className} body`}>
-        <SidebarProvider>
-          <AppSidebar />
-        </SidebarProvider>
+        <AppSidebar />
 
         <div className="body__right">
           <Header />
