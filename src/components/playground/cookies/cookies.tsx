d42d0889ae0cfc2aa5017b9112a509e4a5c22813
Debora Serra <debora.r.serra@gmail.com>
2025-04-16 14:34:59 -0700

    chore: update context usage in cookies

--- src/components/playground/cookies/cookies.tsx
+++ src/components/playground/cookies/cookies.tsx
@@ -6,7 +6,8 @@ import mainStyle from '../playground.module.scss';
 import style from './cookies.module.scss';
 
 const Cookies = () => {
-  const { cookies, setCookies } = usePlayground();
+  const { requestState, setRequestState } = usePlayground();
+  const { cookies } = requestState;
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === ',' || e.key === ' ') {
@@ -17,12 +18,12 @@ const Cookies = () => {
         toast.error('Cookie must be in format: cookie-name=cookie-value');
         return;
       }
-      setCookies([...cookies, ...cookieValue.split(',')]);
+      setRequestState((prev) => ({ ...prev, cookies: [...cookies, ...cookieValue.split(',')] }));
       e.currentTarget.value = '';
     }
     if (e.key === 'Backspace') {
       if (e.currentTarget.value === '') {
-        setCookies(cookies.slice(0, -1));
+        setRequestState((prev) => ({ ...prev, cookies: cookies.slice(0, -1) }));
       }
     }
   };
@@ -32,14 +33,14 @@ const Cookies = () => {
       <label htmlFor="cookies" className={mainStyle['play__label']}>
         Custom cookies{' '}
       </label>
-      <div className={"textfield flex flex-wrap gap-2"}>
+      <div className={'textfield flex flex-wrap gap-2'}>
         {cookies.map((cookie, index) => (
           <span key={index} className="flex items-center gap-1 border border-gray-0/20 rounded-sm px-2 py-1">
             {cookie}
 
             <X
               onClick={() => {
-                setCookies(cookies.filter((_, i) => i !== index));
+                setRequestState((prev) => ({ ...prev, cookies: cookies.filter((_, i) => i !== index) }));
               }}
               className="cursor-pointer"
               size={16}
