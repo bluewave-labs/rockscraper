b99ff03401a1b0a5fd9d086048cd89f2fe56fb8a
Debora Serra <debora.r.serra@gmail.com>
2025-04-16 14:51:35 -0700

    chore: update context usage in url

--- src/components/playground/url.tsx
+++ src/components/playground/url.tsx
@@ -3,13 +3,16 @@ import { Input } from '@bluewavelabs/prism-ui';
 import { usePlayground } from './context';
 
 const Url = () => {
-  const { url, setUrl } = usePlayground();
+  const {
+    requestState: { url },
+    setRequestState,
+  } = usePlayground();
   return (
     <Input
       id="url"
       placeholder="https://"
       value={url}
-      onChange={(e) => setUrl(e.target.value)}
+      onChange={(e) => setRequestState((prev) => ({ ...prev, url: e.target.value }))}
       label="URL"
       variant="label-out"
     />
