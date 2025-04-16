bbb8d8ace5b44fde86e7580b50b6a1702e7c2594
Debora Serra <debora.r.serra@gmail.com>
2025-04-16 10:35:00 -0700

    chore: change headers and cookies position

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,5 +1,7 @@
-'use client'
-import { Card } from '@bluewavelabs/prism-ui';
+'use client';
+import { Button, Card } from '@bluewavelabs/prism-ui';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useState } from 'react';
 import ApiKey from './apiKey';
 import Cookies from './cookies/cookies';
 import Headers from './headers/headers';
@@ -12,14 +14,28 @@ import RightTitle from './rightTitle';
 import Url from './url';
 
 const Playground = () => {
+  const [openFinetune, setOpenFinetune] = useState(false);
+
   return (
     <div className={style.play}>
       <div className={style.play__left}>
-        <h2 className="text-xl text-gray-20 mb-6">Fine tune your query</h2>
-        <Headers />
-        <Cookies />
         <Output />
         <Nodes />
+        <Button
+          variant="link"
+          className="text-xl text-gray-20 mb-6 mt-10 flex justify-between items-center cursor-pointer w-full has-[>svg]:p-0 p-0 hover:no-underline"
+          onClick={() => setOpenFinetune(!openFinetune)}
+        >
+          Fine tune your query {openFinetune ? <ChevronUp /> : <ChevronDown />}
+        </Button>
+        {openFinetune ? (
+          <>
+            <Headers />
+            <Cookies />
+          </>
+        ) : (
+          <></>
+        )}
       </div>
       <Card className={style.play__right}>
         <RightTitle />
