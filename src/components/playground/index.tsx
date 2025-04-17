589035ecb4e69481ad95b926a352d8b80a309a88
Debora Serra <debora.r.serra@gmail.com>
2025-04-17 09:08:45 -0700

    chore: update playground component

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,10 +1,7 @@
 'use client';
-import { Button, Card } from '@bluewavelabs/prism-ui';
-import { ChevronDown, ChevronUp } from 'lucide-react';
-import { useState } from 'react';
+import { Card } from '@bluewavelabs/prism-ui';
 import ApiKey from './apiKey';
-import Cookies from './cookies/cookies';
-import Headers from './headers/headers';
+import FineTune from './fineTune/fineTune';
 import Nodes from './nodes';
 import Output from './output';
 import style from './playground.module.scss';
@@ -14,28 +11,12 @@ import RightTitle from './rightTitle';
 import Url from './url';
 
 const Playground = () => {
-  const [openFinetune, setOpenFinetune] = useState(false);
-
   return (
     <div className={style.play}>
       <div className={style.play__left}>
         <Output />
         <Nodes />
-        <Button
-          variant="link"
-          className="text-xl text-gray-20 mb-6 mt-10 flex justify-between items-center cursor-pointer w-full has-[>svg]:p-0 p-0 hover:no-underline"
-          onClick={() => setOpenFinetune(!openFinetune)}
-        >
-          Fine tune your query {openFinetune ? <ChevronUp /> : <ChevronDown />}
-        </Button>
-        {openFinetune ? (
-          <>
-            <Headers />
-            <Cookies />
-          </>
-        ) : (
-          <></>
-        )}
+        <FineTune />
       </div>
       <Card className={style.play__right}>
         <RightTitle />
