2753c3c9b6254d38c2cf199fe7fbcf848fd795b4
Debora Serra <debora.r.serra@gmail.com>
2025-04-18 14:22:47 -0700

    feat: update extraction schema

--- src/components/playground/fineTune/extractionSchema.tsx
+++ src/components/playground/fineTune/extractionSchema.tsx
@@ -1,81 +1,51 @@
+import PillInput from '@src/components/ui/pillInput';
 import { useScraper } from '@src/utils/context';
 import { ExtractionSchema as ExtractionSchemaType, Request } from '@src/utils/interfaces';
-import { useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
-const ExtractionSchema = () => {
+const ExtractionSchema = ({
+  renderTooltip,
+}: {
+  renderTooltip: (content: ReactNode, className?: string) => ReactNode;
+}) => {
   const {
     requestState: { extractionSchema },
     setRequestState,
   } = useScraper();
-  const [schema, setSchema] = useState<string>('');
+  const [schema, setSchema] = useState<string[]>();
 
-  const updateSchema = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key !== 'Enter' && e.key !== ',') return;
-    e.preventDefault();
-    let error = false;
-    const formattedSchema = schema.split(',').map((item) => {
-      const booleanStarters = [
-        'is',
-        'has',
-        'are',
-        'was',
-        'were',
-        'will',
-        'would',
-        'should',
-        'can',
-        'could',
-        'may',
-        'might',
-        'must',
-        'should',
-        'ought',
-        'need',
-        'want',
-        'expect',
-        'believe',
-        'think',
-        'know',
-        'see',
-        'hear',
-        'smell',
-        'taste',
-        'touch',
-      ];
+  useEffect(() => {
+    const parsedSchema = schema?.map((value) => {
+      const [key, type] = value.split('=');
       return {
-        [item.trim()]: {
-          type: booleanStarters.some((starter) => item.trim().startsWith(starter)) ? 'boolean' : 'string',
+        [key]: {
+          type,
         },
       };
     });
-    if (error) return;
-    setRequestState(
-      (prev) =>
-        ({
-          ...prev,
-          extractionSchema: {
-            type: 'object',
-            properties: formattedSchema,
-          },
-        }) as unknown as Request<ExtractionSchemaType>
-    );
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setSchema(value);
-  };
+    setRequestState((prev) => ({ ...prev, extractionSchema: {
+      ...prev.extractionSchema,
+          properties: parsedSchema,
+        },
+      }) as unknown as Request<ExtractionSchemaType>);
+  }, [schema]);
 
   return (
     <div className="mt-6">
-      <h2 className="mb-4 flex items-center justify-between w-full font-medium text-gray-10">Extraction schema</h2>
-      <textarea
-        className="textfield w-full h-[100px] resize-none mb-4 font-mono text-sm text-gray-0"
-        placeholder={`Enter values for the extraction schema separated by commas. Example: name, price, isActive, items`}
-        value={schema}
-        onChange={handleChange}
-        onKeyDown={updateSchema}
-      ></textarea>
+      <PillInput
+        id="extractionSchema"
+        label="Extraction schema"
+        onChange={(values) => setSchema(values)}
+        placeholder={`Values for the extraction schema.`}
+        initialValues={schema}
+        renderTooltip={renderTooltip}
+        tooltipContent={
+          <p>
+            Add values and it&apos;s type for the extraction schema. <br />
+            Example: name=string, price=number, isActive=boolean, items=array
+          </p>
+        }
+      />
     </div>
   );
 };
